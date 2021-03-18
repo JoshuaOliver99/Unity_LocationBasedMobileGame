8a3d7d4226327538015b113ee0b96b414a4c16c5
Joshua Simpson <56636418+JoshuaOliver99@users.noreply.github.com>
2021-03-18 17:42:51 +0000

    Rearranged the file structure

---
+++ Location Based Mobile Game/Assets/System/Scripts/PetData_SO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/PetDataScriptableObject", order = 1)]
+public class PetData_SO : ScriptableObject
+{
+    // Data
+    //
+    // public inventory inventoryInstance
+}
